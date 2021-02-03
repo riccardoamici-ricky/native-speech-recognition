0007c682d6222439a6e64872393c11b20b2961ec
Riccardo <riccardo@baako.de>
2021-02-03 20:49:47 +0100

    joined the words

--- myscript.js
+++ myscript.js
@@ -8,7 +8,10 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', e => {
-  console.log(e);
+  const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
 })
 
 recognition.start();
